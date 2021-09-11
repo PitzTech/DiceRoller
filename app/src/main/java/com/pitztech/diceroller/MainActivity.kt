eb358fc841999275264169971d20afda6a5a20a8
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-09-11 17:26:41 -0300

    Finish dice rolling

--- app/src/main/java/com/pitztech/diceroller/MainActivity.kt
+++ app/src/main/java/com/pitztech/diceroller/MainActivity.kt
@@ -3,8 +3,10 @@ package com.pitztech.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +20,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
+//        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt: Int = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+//        resultText.text = "$randomInt"
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
